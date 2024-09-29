c6b44f7795c192b556c87e2ad68ad0c251a90850
donroyj95 <manojjayasinghe43@gmail.com>
2024-09-29 22:18:13 +1000

    feature: add user register form

---
+++ src/main/java/controller/HomeController.java
@@ -0,0 +1,36 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import model.Model;
+
+public class HomeController {
+	private Model model;
+	private Stage stage;
+	private Stage parentStage;
+	@FXML
+	private MenuItem viewProfile;
+	@FXML
+	private MenuItem updateProfile;
+	
+	public HomeController(Stage parentStage, Model model) {
+		this.stage = new Stage();
+		this.parentStage = parentStage;
+		this.model = model;
+	}
+	
+	
+	
+	
+	
+	public void showStage(Pane root) {
+		Scene scene = new Scene(root, 500, 300);
+		stage.setScene(scene);
+		stage.setResizable(false);
+		stage.setTitle("Home");
+		stage.show();
+	}
+}
