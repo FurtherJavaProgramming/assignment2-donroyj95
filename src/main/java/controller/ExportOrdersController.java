fab63167068f322e07fef0c2c5f59498aec4551e
donroyj95 <manojjayasinghe43@gmail.com>
2024-10-26 18:24:23 +1100

    feature: update export UI

--- src/main/java/controller/ExportOrdersController.java
+++ src/main/java/controller/ExportOrdersController.java
@@ -1,5 +1,28 @@
 package controller;
 
+import javafx.fxml.FXML;
+import javafx.stage.Stage;
+import model.Model;
+
+import java.awt.*;
+
 public class ExportOrdersController extends MainController{
+
+    @FXML
+    private TextField fileNameField;
+    @FXML
+    private TextField destinationField;
+    @FXML
+    private Button browse;
+    @FXML
+    private Button exportOrders;
+    @FXML
+    private Button back;
+
+
+    public ExportOrdersController(Stage parentStage, Model model) {
+        super(parentStage, model);
+    }
+
 
 }
